0e07e1f56c3cedd8c0f1f8d39a98ee010b50a90d
g-rohan <g.rohan.gowda@gmail.com>
2017-08-09 10:40:50 +0530

    Cleaned up files, changed plugin icon

--- content.js
+++ content.js
@@ -1,9 +1,20 @@
 var captcha_str = $(".captcha img").attr('src');
 var encoded_str = encodeURIComponent(captcha_str);
 
-// https://10.216.55.119:80/predict?captchaString=hello open this site manually in the browser and add it to trusted sites
-uri = "https://10.216.55.119:80/predict?captchaString=" + encoded_str;
-$.get(uri, function( data ) {
-  //alert(data);
-  $("#drcaptcha").val(data)
+var jsonData = { 'captchaString' : encoded_str }
+
+var settings = {
+  "crossDomain": true,
+  "url": "https://nn-captcha.appspot.com/predict",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "datatype": "json",
+  },
+  "processData": false,
+  "data": JSON.stringify(jsonData)
+}
+
+$.ajax(settings).done(function (response) {
+  $("#drcaptcha").val(response)
 });
